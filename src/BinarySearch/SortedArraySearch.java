cff19fbef0d3937017b0def17891b3d227528b4a
Arunkumar C <arunkumar061781@gmail.com>
2026-01-31 23:49:56 +0530

    Added Binary Search Recursive

--- src/BinarySearch/SortedArraySearch.java
+++ src/BinarySearch/SortedArraySearch.java
@@ -7,6 +7,11 @@ public class SortedArraySearch{
         System.out.println(searchInSorted(arr,8));
         System.out.println(searchInSorted(arr,10));
         System.out.println(searchInSorted(arr,-1));
+
+        System.out.println(searchInSortedRecursion(arr,1,0,arr.length-1));
+        System.out.println(searchInSortedRecursion(arr,8,0,arr.length-1));
+        System.out.println(searchInSortedRecursion(arr,10,0,arr.length-1));
+        System.out.println(searchInSortedRecursion(arr,-1,0,arr.length-1));
     }
 
     static int searchInSorted(int[] arr, int target) {
@@ -27,5 +32,21 @@ public class SortedArraySearch{
         }
 
         return -1;
+    }
+
+    static int searchInSortedRecursion(int[] arr, int target,int low,int high) {
+        if(low>high){
+            return -1;
+        }
+        int mid = low + (high - low)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[mid] < target){
+            return searchInSortedRecursion(arr,target,mid+1,high);
+        }
+        else{
+            return searchInSortedRecursion(arr,target,low,mid-1);
+        }
     }
 }
