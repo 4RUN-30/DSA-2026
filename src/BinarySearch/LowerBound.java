a873acd4710aa5320c1623eb30348660b3ebb403
Arunkumar C <arunkumar061781@gmail.com>
2026-02-01 12:53:57 +0530

    Added LowerBound

---
+++ src/BinarySearch/LowerBound.java
@@ -0,0 +1,27 @@
+package BinarySearch;
+
+public class LowerBound {
+    public static void main(String[] args) {
+        int[] arr = {2,3,7,10,11,11,25};
+        System.out.println(lowerBound(arr,11));
+    }
+    static int lowerBound(int[] arr, int target) {
+        // code here
+        int low = 0;
+        int high = arr.length-1;
+        int ans = arr.length;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
